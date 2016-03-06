7e90365194b0dbb873c1594dd72ca1a411e0959c
Liam Marshall <liam@cpan.org>
2016-03-06 15:31:53 -0600

    Yay, finished DAC driver!

--- l_naught/src/mcp4921.h
+++ l_naught/src/mcp4921.h
@@ -10,13 +10,23 @@
 inline void spi_init() {
     DDRB |= _BV(PB1) | _BV(PB2) | _BV(PB3) | _BV(PB5);
 
-    SPCR = _BV(SPE) | _BV(MSTR) | _BV(SPR0);
+    SPCR = _BV(SPE) | _BV(MSTR) | _BV(SPR1);
 }
 
 inline u8 spi_swap_byte(u8 data) {
     SPDR = data;
     loop_until_bit_is_set(SPSR, SPIF);
     return (SPDR);
+}
+
+inline void dac_write(u16 data) {
+    SLAVE_SELECT;
+    data &= 0xfff; // truncate to 12 bits
+
+    spi_swap_byte(_BV(4) | _BV(5) | (data >> 8));
+    spi_swap_byte(data & 0x0ff);
+
+    SLAVE_DESELECT;
 }
 
 #endif
